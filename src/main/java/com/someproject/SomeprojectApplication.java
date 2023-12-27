450a156c8643b869b389a46fb3dd26071f297c5c
[배송정산개발팀]윤민호 <minho.yun@lotte.net>
2023-12-27 14:08:32 +0900

    init

---
+++ src/main/java/com/someproject/SomeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.someproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomeprojectApplication.class, args);
+    }
+
+}
